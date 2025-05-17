ae184d05385a02723ede414ef76b74e1d6a01d5b
haard18 <haardsolanki.itm@gmail.com>
2025-05-17 23:53:45 +0530

    Add @vercel/analytics dependency and integrate Analytics component in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,24 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-     </BrowserRouter>
+      <Analytics />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      {/* </Analytics> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
